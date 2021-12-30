df3c71946c68504a03b84c1f83a930b5d5714414
Tru Lie <84538219+TruLie13@users.noreply.github.com>
2021-12-30 15:59:59 -0700

    changed api url

--- src/config/goth-stock-api.js
+++ src/config/goth-stock-api.js
@@ -1,4 +1,3 @@
 export const baseUrl =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:8000"
-    : "https://cors-anywhere.herokuapp.com/https://goth-stock-backend.herokuapp.com";
+  // "http://localhost:8000"
+  "https://cors-anywhere.herokuapp.com/https://goth-stock-backend.herokuapp.com";
