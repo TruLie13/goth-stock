d52b068adf4a8962a7f1fe54bc6f63736ebf848e
Tru Lie <84538219+TruLie13@users.noreply.github.com>
2021-12-30 17:11:10 -0700

    fixed base_url

--- src/config/goth-stock-api.js
+++ src/config/goth-stock-api.js
@@ -1,3 +1,3 @@
 export const baseUrl =
   // "http://localhost:8000"
-  "https://cors-anywhere.herokuapp.com/https://goth-stock-backend.herokuapp.com";
+  "https://goth-stock-backend.herokuapp.com";
